84db7a64b4d35bb04d33b83a492d87dba34a37ff
abdulGitHb <abdulsamad6876@gmail.com>
2024-02-07 23:13:08 +0530

    client page updated, candidate page a little remaining-2

--- src/app/candidate-form/candidate-benefits/video-resume/page.tsx
+++ src/app/candidate-form/candidate-benefits/video-resume/page.tsx
@@ -2,7 +2,7 @@ const VideoResume = () => {
     return ( <div>
         <h1 className="h-28 bg-[#97144d] flex items-center justify-center bg-gradient-to-b from-gray-200 via-[#97144d]/80 to-gray-200 text-gray-200 ">Video Resume Branding</h1>
         <div className="max-w-[1100px] mx-auto text-2xl text-[#97144d] mt-6 mb-6 font-bold">
-            In today's competitive job market, a standout application is crucial, and our Video Resume Branding solution is the key. Talentworkx offers a cutting-edge platform empowering candidates to create captivating video resumes that leave a lasting impression.
+            In today&apos;s competitive job market, a standout application is crucial, and our Video Resume Branding solution is the key. Talentworkx offers a cutting-edge platform empowering candidates to create captivating video resumes that leave a lasting impression.
             <br />
             Our intuitive tools and customizable templates enable candidates to craft compelling narratives showcasing their personality, skills, and experience. With seamless integration of branding elements, individuals can reinforce their personal brand throughout the video resume.
         </div>
--- src/app/client-form/client-features/hrms/page.tsx
+++ src/app/client-form/client-features/hrms/page.tsx
@@ -5,7 +5,7 @@ const HRMS = () => {
             <div className="max-w-[1100px] mx-auto text-2xl text-[#97144d] mt-6 mb-6 font-bold">
                 Talentworkx offers a cutting-edge HRMS (Human Resource Management System) that revolutionizes the way organizations manage their workforce. Our comprehensive HRMS module streamlines core HR processes, including employee data management, payroll, attendance tracking, and leave management. With intuitive interfaces and customizable features, Talentworkx empowers HR teams to automate routine tasks, reduce administrative burdens, and focus on strategic initiatives. 
                 <br />
-                Whether it's simplifying onboarding processes for new hires or generating insightful reports for informed decision-making, our HRMS module enhances operational efficiency and employee satisfaction. With Talentworkx, HRMS becomes more than just a software solution; it becomes a strategic partner in driving organizational success.</div>
+                Whether it&apos;s simplifying onboarding processes for new hires or generating insightful reports for informed decision-making, our HRMS module enhances operational efficiency and employee satisfaction. With Talentworkx, HRMS becomes more than just a software solution; it becomes a strategic partner in driving organizational success.</div>
         </div>
      );
 }
